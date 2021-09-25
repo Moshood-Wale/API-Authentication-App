561347ba4d9ada766c9ffbd61985f8314087c43a
Moshood Salawudeen <86296143+Moshood-Wale@users.noreply.github.com>
2021-09-25 11:29:31 +0100

    Update settings.py

--- authentication_api/settings.py
+++ authentication_api/settings.py
@@ -153,7 +153,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 
 EMAIL_PORT = 587
 
-EMAIL_HOST_USER = 'moshood815@gmail.com'
+EMAIL_HOST_USER = ''
 
-EMAIL_HOST_PASSWORD = 'adewale815'
+EMAIL_HOST_PASSWORD = ''
 
